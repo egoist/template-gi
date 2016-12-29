c312a6257453abc4b3d520de69164400b5a2debb
EGOIST <0x142857@gmail.com>
2016-12-29 22:36:31 +0800

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# gitignore
+
+Generate `.gitignore` file in your project.
+
+![preview.gif](./media/preview.gif)
+
+## Usage
+
+```bash
+cd your-project
+sao gi
+```
+
+## Available gitignore types
+
+- default
+- gitbook
+
+Check out [./template](./template) for more details.
+
+
+## License
+
+MIT &copy; EGOIST
---
+++ sao.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const path = require('path')
+
+const types = fs.readdirSync(path.join(__dirname, 'template'))
+  .filter(v => /^gi-/.test(v))
+  .map(v => v.substring(3))
+
+const filters = {}
+types.forEach(type => {
+  filters[`gi-${type}`] = `type === "${type}"`
+})
+
+module.exports = {
+  enforceCurrentFolder: true,
+  prompts: {
+    type: {
+      type: 'list',
+      message: 'Choose a gitignore file:',
+      choices: types
+    }
+  },
+  move: {
+    'gi-*': '.gitignore'
+  },
+  filters: filters,
+  post({chalk, folderName, data, log}) {
+    log.success(`${data.type} .gitignore is successfully generated in ${folderName}`)
+  }
+}
